6dcf9a49d2a9fe33968f81c936dece922023e54c
Stephen Balogh <sgbalogh@gmail.com>
2015-09-30 18:26:51 -0400

    adds comments to lab4

--- labs/balogh_lab4.java
+++ labs/balogh_lab4.java
@@ -1,64 +1,73 @@
 /**
- * Created by stephen on 9/30/15.
+ * Created by Stephen Balogh on 9/30/15.
  */
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 public class balogh_lab4 {
     public static void main(String[] args){
-        int inputX = 0;
-        int inputY = 0;
+
+        /* Begin declaring variables and setting initial values. */
+
         char carColorChar = randomColor();
-        Scanner userInput = new Scanner(System.in);
-        int userSelection = 0;
+        Scanner userInput = new Scanner(System.in); // This object used for all user-input
+        int userSelection = 0; // For first-level menu options during while-loop
         boolean dontExit = true; // Controller for while-loop
-        String userDirection = "";
-
-        // Initial car value determination
-        int locX = (randPosition());
-        int locY = (randPosition());
+        String userDirection = ""; // Used during second-level menu option, nested under "change position"
+        int locX = (randPosition()); // Assign initial X value using randPosition()
+        int locY = (randPosition()); // Assign initial Y value using randPosition()
         int distanceMove = 0;
-        boolean ignition = false;
-        // End allocation of initial values
+        boolean ignition = false; // Assign initial ignition value
 
-        // Display of initial values
+        /* End of variable declaration and value allocation. */
+
         System.out.print("* Initial Vehicle State *\n");
-        report(carColorChar, ignition, locX, locY);
-        // End display
+        report(carColorChar, ignition, locX, locY); // Generate initial report (display status of vehicle and location on map)
+
+        /* Begin while-loop, will allow user to change position, ignition unlimited number
+         * of times until they EXIT program by passing in option '3' at the main menu. */
+
         while (dontExit) {
-            System.out.println("\nSo what do you want to do now?\n1. toggle the ignition on/off\n2. change the position of the car\n3. quit this program");
-            userSelection = userInput.nextInt();
+            System.out.println("\nSo what do you want to do now?\n1. toggle the ignition on/off\n" + "" +
+                    "2. change the position of the car\n3. quit this program");
+            userSelection = userInput.nextInt(); // Collect selection from user
             switch (userSelection) {
                 case 1:
-                    ignition = toggleIgnition(ignition);
+                    ignition = toggleIgnition(ignition); // Use toggleIgnition() to change swap value
                     break;
                 case 2:
+                    /* Uses do/while-loop to ensure that user input is one of two acceptable options, else
+                     * they will be asked again. */
                     do {
                         System.out.println("Ok. Move in which direction?\nH. Horizontal\nV. Vertical");
-                        userDirection = userInput.next();
+                        userDirection = userInput.next(); // Gathers second-level menu option
                     } while (!(userDirection.equals("H") || userDirection.equals("V")));
+
                     if (userDirection.equals("H")) {
-                        System.out.println("Move horizonally how far? Negative values for left, positive for right.");
+                        System.out.println("Move horizontally how far? Enter negative values for left, positive for right.");
                         distanceMove = userInput.nextInt();
                         System.out.println("Attempting to move horizontally, " + distanceMove + " steps.");
-                        locX = moveHorizontal(locX, distanceMove, ignition);
+                        locX = moveHorizontal(locX, distanceMove, ignition); // Method returns new X value
                     } else if (userDirection.equals("V")) {
-                        System.out.println("Move vertically how far? Negative values for up, positive for down.");
+                        System.out.println("Move vertically how far? Enter negative values for up, positive for down.");
                         distanceMove = userInput.nextInt();
                         System.out.println("Attempting to move vertically, " + distanceMove + " steps.");
-                        locY = moveVertical(locY, distanceMove, ignition);
+                        locY = moveVertical(locY, distanceMove, ignition); // Method returns new Y value
                     }
                     break;
                 case 3:
-                    System.out.println("Adios.");
-                    dontExit = false;
+                    System.out.println("\nAdios."); // Exit message
+                    dontExit = false; // Switches controller of while-loop to false
                     break;
             }
             if (dontExit) {
-                report(carColorChar, ignition, locX, locY);
+                report(carColorChar, ignition, locX, locY); // Conditional used to omit final report when exiting
             }
         }
     }
 
     public static char randomColor() {
+        /* Uses random number generator, casts result to integer, then assigns characters representing color
+         * of car to each of the five possible integers (0-4). */
         int colorChoose = (int)(Math.random() * 5.00);
         char carColorChar = ' ';
         switch (colorChoose) {
@@ -84,9 +93,13 @@ public class balogh_lab4 {
     }
 
     public static void report(char carColorChar, boolean ignition, int x, int y) {
+        /* Used to generate report of car status, including its color, ignition state, grid position, and
+        * a graphical representation of that grid position. Returns nothing, but prints directly to console
+        * when invoked. */
         System.out.println();
         String carColorString = "";
         switch (carColorChar) {
+            /* Associates a color character with the name of that color as a String. */
             case 'R':
                 carColorString = "red";
                 break;
@@ -106,6 +119,8 @@ public class balogh_lab4 {
 
         System.out.println("Color: " + carColorString + "\nIgnition status: " + ignition + "\nLocation: (X: " + x + ", Y: " + y + ")\n");
 
+        /* Begin grid position drawing. */
+
         for (int a = 1; a < 21; a++) {
             if (a == y) {
                 for (int b = 1; b < 21; b++) {
@@ -120,18 +135,21 @@ public class balogh_lab4 {
                 System.out.println("- - - - - - - - - - - - - - - - - - - -");
             }
         }
-        return;
     }
 
     public static int randPosition(){
+        /* Generates a random integer value from 1-20. */
         return ((int)(Math.random() * 20.00)) + 1;
     }
 
     public static boolean toggleIgnition(boolean currentIgnition) {
+        /* Gives inverse of current ignition state. */
         return !currentIgnition;
     }
 
     public static int moveHorizontal(int x, int distanceMove, boolean ignition) {
+        /* Moves vehicle horizontally given an initial position and distance to move, assuming ignition is 'true';
+         * will only return a new integer position if the value does not go over the grid dimensions. */
         int newPos = 0;
         if (ignition) {
             if ( ((x + distanceMove) <= 20 ) && ((x + distanceMove) >= 1 ) ) {
@@ -147,6 +165,7 @@ public class balogh_lab4 {
     }
 
     public static int moveVertical(int y, int distanceMove, boolean ignition) {
+        /* Does the exact same thing as moveHorizontal(). */
         int newPos = 0;
         if (ignition) {
             if ( ((y + distanceMove) <= 20 ) && ((y + distanceMove) >= 1 ) ) {
